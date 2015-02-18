c75b082ec97431e6e16454ca6fc07bcdfc6f4524
Gregoire Astruc <gregoire.astruc@gmail.com>
2015-02-18 09:12:49 +0100

    Updated assembly version and copyright notice.

--- ENetCS/Properties/AssemblyInfo.cs
+++ ENetCS/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ENet")]
 [assembly: AssemblyProduct("ENet for C#")]
-[assembly: AssemblyCopyright("Copyright © 2011-2013 James F. Bellinger <jfb@zer7.com>")]
-[assembly: AssemblyVersion("1.3.6.3")]
-[assembly: AssemblyFileVersion("1.3.6.3")]
+[assembly: AssemblyCopyright("Copyright © 2011-2013 James F. Bellinger <jfb@zer7.com>, 2015 Gregoire Astruc <gregoire.astruc@gmail.com>")]
+[assembly: AssemblyVersion("1.3.12.0")]
+[assembly: AssemblyFileVersion("1.3.12.0")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
